884d3ad552cc2ff248641cf0d5d3bed23a8c04b1
MissNoodles <cldxy23587@163.com>
2022-11-25 10:12:15 +0800

    Create index.js

---
+++ src/config/index.js
@@ -0,0 +1,15 @@
+const CesiumToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiIyNDY1NmZiNS0wYmU1LTRkNTgtODA0Yi1jMzI0NGE4ZTA2ZjciLCJpZCI6NTc3ODksImlhdCI6MTYyMjYxNzA4OX0.1sRx-WfaOPxGx-ogkElxlWmbRwUa_8oiT9hQgsIvvjk';
+// 诗人路线颜色
+const colorList = ['#FFE9A0','#367E18','#F57328','#CC3636','#FA7070','#FBF2CF','#C6EBC5','#A1C298','#6E85B7','#B2C8DF','#C4D7E0','#F8F9D7','#CDF0EA','#F9F9F9'];
+const cesium_css_url = "https://cesium.com/downloads/cesiumjs/releases/1.97/Build/Cesium/Widgets/widgets.css";
+const cesiumJS_url = 'https://cesium.com/downloads/cesiumjs/releases/1.97/Build/Cesium/Cesium.js';
+const tiandiMap_cva_url = "http://{s}.tianditu.gov.cn/cva_w/wmts?service=wmts&request=GetTile&version=1.0.0&LAYER=cva&tileMatrixSet=w&TileMatrix={TileMatrix}&TileRow={TileRow}&TileCol={TileCol}&style=default.jpg&tk=6b79a2f2c19c473918f03903426564ca";
+const model_url = "http://101.34.228.44:3003/public/model.gltf";
+export {
+	CesiumToken,
+	colorList,
+	cesium_css_url,
+	cesiumJS_url,
+	tiandiMap_cva_url,
+	model_url,
+}
