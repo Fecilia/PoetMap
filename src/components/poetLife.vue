76083d821a299a1561d3703537811401ac4d1368
MissNoodles <cldxy23587@163.com>
2022-11-25 10:10:06 +0800

    Create poetLife.vue

---
+++ src/components/poetLife.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { userStore } from '../stores/modules/user';
+import { computed } from 'vue';
+import { ElPopover, ElButton } from 'element-plus';
+import { Pointer } from '@element-plus/icons-vue'
+
+const store = userStore();
+
+// 诗人简介
+const poetLife = computed(() => {
+  return store.introduction;
+})
+
+</script>
+
+<template>
+  <div v-show="poetLife">
+    <el-popover
+      placement="right"
+      title="简介"
+      :width="200"
+      trigger="hover"
+      :content="poetLife"
+      >
+      <template #reference>
+        <el-button type="warning" circle :icon="Pointer"></el-button>
+      </template>
+    </el-popover>
+  </div>
+</template>
+
+<style scoped lang="sass">
+</style>
