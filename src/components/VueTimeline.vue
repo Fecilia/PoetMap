278c6d91f6baf9e9597a0e66598e9a82fe1c4347
MissNoodles <cldxy23587@163.com>
2022-11-25 10:09:28 +0800

    Create VueTimeline.vue

---
+++ src/components/VueTimeline.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id='timeline'></div>
+</template>
+
+<script>
+import * as d3 from 'd3'
+import timeline from '../graph/timeline'
+
+export default {
+  props: {
+    data: {
+      type: Array
+    },
+    config: {
+      type: Object,
+      default: () => { }
+    }
+  },
+  mounted() {
+    d3.select('#timeline')
+      .datum(this.data)
+      .call(timeline({
+        widthResizable: true,
+        viewHeight: 300,
+        margin: {
+          top: 0,
+          bottom: 30,
+          left: 15,
+          right: 15
+        },
+        onEventClick: this.config?.onEventClick
+      }))
+  }
+}
+</script>
